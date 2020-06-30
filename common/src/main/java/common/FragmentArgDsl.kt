3db628653320df970e307f162e730fa182b15864
Rostislav Chekan <r.chekan@gmail.com>
2020-06-30 16:57:11 +0300

    Better fragment arguments dsl

--- common/src/main/java/common/FragmentArgDsl.kt
+++ common/src/main/java/common/FragmentArgDsl.kt
@@ -3,9 +3,11 @@
 package common
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import java.io.Serializable
 import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty1
 
 open class FragmentArg<T>(
         private inline val getter: Bundle.(String) -> T,
@@ -20,10 +22,25 @@ open class FragmentArg<T>(
 }
 
 inline fun <T : Serializable?> Fragment.serializable() = FragmentArg({ getSerializable(it) as T }, { name, value -> this.putSerializable(name, value) })
+inline fun <T : Parcelable?> Fragment.parcelable() = FragmentArg({ getParcelable<T>(it) }, { name, value -> this.putParcelable(name, value) })
 inline fun Fragment.int(defValue: Int = 0) = FragmentArg({ getInt(it, defValue) }, { name, value -> this.putInt(name, value) })
 inline fun Fragment.boolean(defValue: Boolean = false) = FragmentArg({ getBoolean(it, defValue) }, { name, value -> this.putBoolean(name, value) })
 inline fun <T : Serializable?> Fragment.string(defValue: String? = null) = FragmentArg({ getString(it, defValue) }, { name, value -> this.putString(name, value) })
 
-inline fun <T : Fragment> T.arguments(block: Bundle.() -> Unit) = this.apply {
-    arguments = Bundle().apply(block)
+inline fun <T : Fragment> T.arguments(block: FragmentArgDsl.() -> Unit) = this.apply {
+    arguments = FragmentArgDsl(Bundle()).apply(block).bundle
+}
+
+class FragmentArgDsl(val bundle: Bundle) {
+    operator fun <F : Fragment, T> KProperty1<F, T?>.plusAssign(value: T) {
+        when (value) {
+            is Parcelable   -> bundle.putParcelable(name, value)
+            is Serializable -> bundle.putSerializable(name, value)
+            is Int          -> bundle.putInt(name, value)
+            is Boolean      -> bundle.putBoolean(name, value)
+            is String       -> bundle.putString(name, value)
+            is Long         -> bundle.putLong(name, value)
+            else            -> throw IllegalArgumentException()
+        }
+    }
 }
