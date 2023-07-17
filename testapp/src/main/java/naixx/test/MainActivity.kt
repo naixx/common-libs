0d403b2af562b93f16e7003248f6e96c15dae112
Rostislav Chekan <r.chekan@gmail.com>
2023-07-17 19:28:37 +0300

    Update timber

--- testapp/src/main/java/naixx/test/MainActivity.kt
+++ testapp/src/main/java/naixx/test/MainActivity.kt
@@ -2,15 +2,14 @@ package naixx.test
 
 import android.app.Activity
 import android.os.Bundle
-import com.github.naixx.L
-import com.github.naixx.trees.ConsoleTree
 import com.github.naixx.trees.CrashlyticsTree
-import timber.log.Timber
+import timber.log.L
 
 class MainActivity:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Timber.plant(CrashlyticsTree())
+       // L.plant(CrashlyticsTree())
+        L.plant(L.DebugTree())
 
         L.e("qwe")
     }
